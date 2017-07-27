26aa7b933b1e2ea8ca5985e696ebc4a0e75814e3
ozdemirali <ozdemirali@hotmail.com>
2017-07-27 20:18:48 +0300

    Temel ayarlar yapıldı

---
+++ app/index.js
@@ -0,0 +1,11 @@
+require('bootstrap/dist/css/bootstrap.min.css');
+require('./css/main.css'); 
+import Vue from 'vue';
+
+
+new Vue({
+    el:'#app',
+    data:{
+        message:"Hello Vue"
+    }
+});
